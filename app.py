b89884cb97e148f51862b5a2989e6e04cafc2634
iamsusiep <iamsusie@gmail.com>
2018-04-16 21:18:17 -0400

    finished up to task 4

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+# app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template('index.html')
+	return render_template('index.html')
 
+@app.route('/sp3581')
+def sp3581():
+	return render_template('sp3581.html')
 
 if __name__ == '__main__':
     app.run()
+
+
