ed0d8ee7d5f4a92694921c3c1cd70780084d78e4
iamsusiep <iamsusie@gmail.com>
2018-04-18 23:51:34 -0400

    pokeapi task completed

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 # app.config['DEBUG'] = True
@@ -10,8 +12,20 @@ def main():
 @app.route('/sp3581')
 def sp3581():
 	return render_template('sp3581.html')
+	
+@app.route('/pokemon/<query>')
+def pokemon(query):
+	try:
+		url ='http://pokeapi.co/api/v2/pokemon/' + query
+		response = requests.request("GET", url, data="")
+		data = response.json()
+		# return str(data.keys())
+		query_d = int(query)
+		return "The pokemon with id "+str(query)+ " is " + str(data['name']) 
+	except ValueError as vr:
+		return str(query)+ " has id " + str(data['name'])
+	pass
 
 if __name__ == '__main__':
     app.run()
-
 
